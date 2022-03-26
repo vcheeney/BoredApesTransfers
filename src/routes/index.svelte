9765c5c2efa66743bb2eb718d5140bc20fe79e19
Victor Cheeney <victorcheeney@gmail.com>
2022-03-26 07:52:03 -0400

    Added some information text

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,7 +7,9 @@
 	import { formatAccountString } from '../utils/utils';
 
 	onMount(async () => {
-		await defaultEvmStores.setProvider();
+		if (window.ethereum) {
+			await defaultEvmStores.setProvider(window.ethereum);
+		}
 	});
 
 	let transfers: NftTransfer[] = [];
@@ -54,10 +56,19 @@
 	$: console.log('transfers', transfers);
 </script>
 
-<div class="bg-orange-500 p-4 h-screen overflow-y-scroll">
+<div class="bg-orange-600 p-4 h-screen overflow-y-scroll text-white font-semibold">
 	<div class="container max-w-screen-sm mx-auto">
 		{#if $connected}
-			<h1 class="text-3xl pb-4 text-white font-extrabold text-center">Bored Apes Transfers</h1>
+			<header class="pb-4 text-center">
+				<h1 class="text-3xl text-white font-extrabold mb-2">Bored Apes Transfers</h1>
+				<p>
+					This app listens to <a
+						class="underline"
+						href="https://etherscan.io/address/0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d#events"
+						target="blank">Transfer events</a
+					> on the BYAC smart contract. New transfers will be added to the top of the list as they happen.
+				</p>
+			</header>
 			<div class="space-y-4">
 				{#each transfers as transfer}
 					<div class="bg-white rounded-xl overflow-hidden flex shadow-md">
@@ -72,7 +83,17 @@
 				{/each}
 			</div>
 		{:else}
-			<h1 class="text-3xl pb-4 text-white font-extrabold text-center">Not connected...</h1>
+			<div class="pb-4 text-center">
+				<h1 class="text-3xl mb-2 text-white font-extrabold text-center">Not connected...</h1>
+				<p>
+					This app requires a Web3 provider to be connected. Please make sure you have <a
+						class="underline"
+						href="https://metamask.io/"
+						target="blank">MetaMask</a
+					>
+					installed.
+				</p>
+			</div>
 		{/if}
 	</div>
 	<div />
